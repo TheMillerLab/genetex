aadb8108f0379b02aa6a88bb606de1b970025573
TheMillerLab <80731901+TheMillerLab@users.noreply.github.com>
2021-08-11 16:50:12 -0400

    Add files via upload

--- R/gene.variants.transform.for.redcap.R
+++ R/gene.variants.transform.for.redcap.R
@@ -32,13 +32,34 @@ gene.variants.transform.for.redcap <- function(data){
   dt.2 <- genetex::link.gene.variant.variables(data = dt.1)
 
   # Create a grouping system based on gene names
-  dt.3 <- dt.2 %>% dplyr::mutate(keywords = stringr::str_detect(dt.2$genes_variants, pattern = regex(genes_boundary_regex)),
+  dt.3 <- dt.2 %>% dplyr::mutate(keywords = stringr::str_detect(dt.2$genes_variants,
+                                                                pattern = regex(genes_boundary_regex)),
                                  group = cumsum(keywords))
 
   # paste "group" and "var" to create a "variable" column, which are the variable names that are used in the Genomics Instrument
   dt.4 <- dt.3 %>% mutate(variables = paste(var, group, sep = "_"))
+
   # Select appropriate columns and rename
-  dt.final <- dt.4 %>% select(variables, genes_variants) %>% rename(results = genes_variants)
+  dt.5 <- dt.4 %>% select(variables, genes_variants) %>% rename(results = genes_variants)
+  # Create a grouping system based on gene names so we can arrange in a moment
+  dt.6 <- dt.5 %>%
+    dplyr::mutate(keywords = stringr::str_detect(string = dt.5$results,
+                                                 pattern = stringr::regex(genes_boundary_regex)),
+                  group = base::cumsum(keywords))
+  # TERT promoter variants are often reported as a conglomerate of multiple alterations. This can lead to duplicates of
+  ## variable names, which cause the algorithm to error. Thus, we should aggregate these in one cell.
+  ### Let's do that by transposeing wide via paste0
+  dt.7 <- dt.6 %>% dplyr::group_by(variables) %>% dplyr::mutate(merge = base::paste0(results, collapse = "; ")) %>% ungroup()
+
+  # slice to eliminate duplicates
+  ## first, we'll make one more merge, so that we don't eliminate two rows with the same gene name that we
+  ### actually want to keep (e.g. have two different mutations that we want to capture)
+  dt.8a <- dt.7 %>% group_by(variables) %>% dplyr::mutate(merge_2 = base::paste(group, merge, sep = "-"))
+  dt.8 <- dt.8a %>% dplyr::group_by(merge_2) %>% dplyr::slice_head() %>% ungroup()
+
+  # Order by group to maintain the order in the report
+  dt.9 <- dt.8 %>% dplyr::arrange(group, variables)
+  dt.final <- dt.9 %>% select(variables, merge) %>% rename(results = merge)
 
   # Add variants_number
   variants.number.1 <- stringr::str_detect(string = dt.final$variables, pattern = "variant_gene")
